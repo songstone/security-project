6d6e7ca5913eb18d1e5674bfc33acb2fb5c9c066
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-02-01 18:34:42 +0900

    feat: create dto
    - UserRegisterDto
    - NoteRegisterDto

---
+++ src/main/java/com/example/securityproject/dto/UserRegisterDto.java
@@ -0,0 +1,13 @@
+package com.example.securityproject.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class UserRegisterDto {
+
+    private String userName;
+    private String password;
+}
