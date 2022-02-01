d57a6a0ea86ff98fc816b1e22a2601a1871cd126
Song Seung-hyun <thdtmdgus5505@gmail.com>
2022-02-01 18:35:25 +0900

    feat: implement controller
    - SignUpController
    - NoteController
    - NoticeController
    - AdminController

---
+++ src/main/java/com/example/securityproject/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.example.securityproject.controller;
+
+import com.example.securityproject.domain.note.Note;
+import com.example.securityproject.service.NoteService;
+import com.example.securityproject.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final NoteService noteService;
+
+    @GetMapping
+    public String getNoteForAdmin(Authentication authentication, Model model){
+        User user = (User) authentication.getPrincipal();
+        List<Note> notes = noteService.findByUser(user);
+        model.addAttribute("notes", notes);
+        return "admin/index";
+    }
+
+
+}
